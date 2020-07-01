29936e28314285974d87278e24182db4d924fa43
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-07-01 14:00:01 -0700

    Improve the Dross information

--- src/main/java/org/bonitasoft/truckmilk/plugin/MilkRadarBonitaEngine.java
+++ src/main/java/org/bonitasoft/truckmilk/plugin/MilkRadarBonitaEngine.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.bonitasoft.log.event.BEvent;
+import org.bonitasoft.log.event.BEvent.Level;
+import org.bonitasoft.log.event.BEventFactory;
+import org.bonitasoft.radar.Radar;
+import org.bonitasoft.radar.Radar.RadarPhotoParameter;
 import org.bonitasoft.radar.Radar.RadarPhotoResult;
 import org.bonitasoft.radar.Radar.RadarResult;
 import org.bonitasoft.radar.RadarFactory;
@@ -11,19 +16,19 @@ import org.bonitasoft.radar.RadarPhoto;
 import org.bonitasoft.radar.RadarPhoto.IndicatorPhoto;
 import org.bonitasoft.radar.connector.RadarTimeTrackerConnector;
 import org.bonitasoft.radar.connector.RadarTimeTrackerConnector.TimeTrackerParameter;
+import org.bonitasoft.radar.process.RadarCase;
+import org.bonitasoft.radar.process.RadarProcess;
+import org.bonitasoft.radar.process.RadarProcess.ParameterProcess;
 import org.bonitasoft.radar.workers.RadarWorkers;
-import org.bonitasoft.log.event.BEvent;
-import org.bonitasoft.log.event.BEvent.Level;
 import org.bonitasoft.truckmilk.engine.MilkJobOutput;
 import org.bonitasoft.truckmilk.engine.MilkPlugIn;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription.CATEGORY;
-import org.bonitasoft.truckmilk.engine.MilkPlugIn.PlugInParameter;
-import org.bonitasoft.truckmilk.engine.MilkPlugIn.TypeParameter;
+import org.bonitasoft.truckmilk.engine.MilkPlugInToolbox;
 import org.bonitasoft.truckmilk.engine.MilkPlugInToolbox.DelayResult;
+import org.bonitasoft.truckmilk.job.MilkJob;
 import org.bonitasoft.truckmilk.job.MilkJob.ExecutionStatus;
 import org.bonitasoft.truckmilk.job.MilkJobContext;
-import org.bonitasoft.truckmilk.job.MilkJob;
 import org.bonitasoft.truckmilk.job.MilkJobExecution;
 import org.bonitasoft.truckmilk.toolbox.MilkLog;
 
@@ -36,16 +41,20 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
     private static BEvent eventErrorNoRadarTrackerConnector = new BEvent(MilkRadarBonitaEngine.class.getName(), 1, Level.ERROR,
             "Tracker Connector radar not found", "The Radar Tracker Connector is not found", "Connector Too Long can't be calculated", "Check library and dependency");
     private static BEvent eventConnectorTooLongStatus = new BEvent(MilkRadarBonitaEngine.class.getName(), 2, Level.INFO,
-            "Connector", "Status on the connector too long radar" );
+            "Connector", "Status on the connector too long radar");
 
     private static PlugInParameter cstParamMonitorConnector = PlugInParameter.createInstance("Connector", "Connector Too Long", TypeParameter.BOOLEAN, true, "Monitor connector execution. When an execution is too long, return it");
     private static PlugInParameter cstParamConnectorDurationInSecond = PlugInParameter.createInstance("Connectorduration", "Connector Limit Duration (seconds)", TypeParameter.LONG, 50L, "Report all connector execution longer than this value");
-    private static PlugInParameter cstParamConnectorFrame = PlugInParameter.createInstanceDelay("Connectframe", "Connector Frame (in minutes)", DELAYSCOPE.HOUR,12, "Search in all connector execution between now and now - frame");
+    private static PlugInParameter cstParamConnectorFrame = PlugInParameter.createInstanceDelay("Connectframe", "Connector Frame (in minutes)", DELAYSCOPE.HOUR, 12, "Search in all connector execution between now and now - frame");
 
     private static PlugInParameter cstParamMonitorWorker = PlugInParameter.createInstance("Worker", "Workers", TypeParameter.BOOLEAN, true, "Monitor workers. Return number of workers running, waiting");
 
-    private final static PlugInMeasurement cstMesureConnectorConnectorCall = PlugInMeasurement.createInstance(RadarTimeTrackerConnector.CSTPHOTO_CONNECTORSCALL, "Number of connector called", "Give the number of connector called in the period");
-    private final static PlugInMeasurement cstMesureConnectorConnectorOverloaded = PlugInMeasurement.createInstance(RadarTimeTrackerConnector.CSTPHOTO_CONNECTORSOVERLOADED, "Number of connector with a long execution", "Number of connectors with a long execution");
+    private static PlugInParameter cstParamStatisticsProcess = PlugInParameter.createInstance("Process", "Process", TypeParameter.BOOLEAN, true, "Statistics on process (Number of process, number of active case)");
+    private static PlugInParameter cstParamProcessDelayDeployment =   PlugInParameter.createInstanceDelay("ProcessDeployment", "Delay to collect processes deployed in this time frame", DELAYSCOPE.WEEK, 1, "Collect processes deployed in this delay");
+
+
+    private final static PlugInMeasurement cstMesureConnectorConnectorCall = PlugInMeasurement.createInstance(RadarTimeTrackerConnector.CSTPHOTO_CONNECTORSCALL, "Number of connector called", "Give the number of connector called in the period").withTypeMeasure(TypeMeasure.LONG);
+    private final static PlugInMeasurement cstMesureConnectorConnectorOverloaded = PlugInMeasurement.createInstance(RadarTimeTrackerConnector.CSTPHOTO_CONNECTORSOVERLOADED, "Number of connector with a long execution", "Number of connectors with a long execution").withTypeMeasure(TypeMeasure.LONG);
 
     public MilkRadarBonitaEngine() {
         super(TYPE_PLUGIN.EMBEDED);
@@ -58,8 +67,7 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
         plugInDescription.setLabel("Radar Bonita Engine");
         plugInDescription.setExplanation("Monitor different indicators");
         plugInDescription.setCategory(CATEGORY.MONITOR);
-        
-        
+
         plugInDescription.addParameterSeparator("Radar Connector", "Monitor the connector activity");
         plugInDescription.addParameter(cstParamMonitorConnector);
         plugInDescription.addParameter(cstParamConnectorDurationInSecond);
@@ -67,9 +75,14 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
 
         plugInDescription.addParameterSeparator("Radar Worker", "Workers activity");
         plugInDescription.addParameter(cstParamMonitorWorker);
+
+        plugInDescription.addParameterSeparator("Statistics", "Statistics activity");
+        plugInDescription.addParameter(cstParamStatisticsProcess);
+        plugInDescription.addParameter(cstParamProcessDelayDeployment);
         
         plugInDescription.addMesure(cstMesureConnectorConnectorCall);
         plugInDescription.addMesure(cstMesureConnectorConnectorOverloaded);
+
         return plugInDescription;
     }
 
@@ -89,24 +102,25 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
 
     @Override
     public MilkJobOutput execute(MilkJobExecution jobExecution) {
-        MilkJobOutput plugTourOutput = jobExecution.getMilkJobOutput();
+        MilkJobOutput milkJobOutput = jobExecution.getMilkJobOutput();
 
-        plugTourOutput.executionStatus = ExecutionStatus.SUCCESS;
+        milkJobOutput.executionStatus = ExecutionStatus.SUCCESS;
 
         // we return a SUCCESSNOTHING is nothing is visible on radars
-        boolean everythingIsCalm=true;
-        
+        boolean everythingIsCalm = false;
+        everythingIsCalm=true;
+
         // How many flownode do we have to re-execute?
         RadarFactory radarFactory = RadarFactory.getInstance();
         List<RadarPhoto> listPhoto = new ArrayList<>();
-        plugTourOutput.executionStatus = ExecutionStatus.SUCCESS;
+        milkJobOutput.executionStatus = ExecutionStatus.SUCCESS;
 
         if (Boolean.TRUE.equals(jobExecution.getInputBooleanParameter(cstParamMonitorConnector))) {
             RadarTimeTrackerConnector radarTimeTrackerConnector = (RadarTimeTrackerConnector) radarFactory.getInstance(RADAR_NAME_CONNECTORTIMETRACKER, RadarTimeTrackerConnector.CLASS_RADAR_NAME, jobExecution.getTenantId(), jobExecution.getApiAccessor());
 
             if (radarTimeTrackerConnector == null) {
-                plugTourOutput.addEvent(new BEvent(eventErrorNoRadarTrackerConnector, "Radar Worker[" + RadarWorkers.CLASS_RADAR_NAME + "] not found"));
-                plugTourOutput.executionStatus = ExecutionStatus.ERROR;
+                milkJobOutput.addEvent(new BEvent(eventErrorNoRadarTrackerConnector, "Radar Worker[" + RadarWorkers.CLASS_RADAR_NAME + "] not found"));
+                milkJobOutput.executionStatus = ExecutionStatus.ERROR;
             } else {
 
                 // update the configuration
@@ -121,51 +135,82 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
                 boolean isActivated = radarTimeTrackerConnector.isActivated().isActivated;
                 if (!isActivated) {
                     RadarResult radarResult = radarTimeTrackerConnector.activate();
-                    plugTourOutput.addEvents(radarResult.listEvents);
+                    milkJobOutput.addEvents(radarResult.listEvents);
                     isActivated = radarResult.isActivated;
                 }
 
                 // take a photo now
                 if (isActivated) {
-                    
+
                     // Take the photo
-                    TimeTrackerParameter timeTrackerParameter = new TimeTrackerParameter(); 
+                    TimeTrackerParameter timeTrackerParameter = new TimeTrackerParameter();
                     RadarPhotoResult radarPhotoResult = radarTimeTrackerConnector.takePhoto(timeTrackerParameter);
-                    
+
                     listPhoto.addAll(radarPhotoResult.listPhotos);
-                    plugTourOutput.addEvents(radarPhotoResult.listEvents);
+                    milkJobOutput.addEvents(radarPhotoResult.listEvents);
 
                     // set up different mesure
-                    
-                    int connectorCall = (int) addMesureFromIndicator(cstMesureConnectorConnectorCall, radarPhotoResult, plugTourOutput);
-                    int connectorOverload = (int) addMesureFromIndicator(cstMesureConnectorConnectorOverloaded, radarPhotoResult, plugTourOutput);
-                    
-                    if (connectorCall>0 || connectorOverload>0)
-                        everythingIsCalm=false;
-                    
+
+                    int connectorCall = (int) addMesureFromIndicator(cstMesureConnectorConnectorCall, radarPhotoResult, milkJobOutput);
+                    int connectorOverload = (int) addMesureFromIndicator(cstMesureConnectorConnectorOverloaded, radarPhotoResult, milkJobOutput);
+
+                    if (connectorCall > 0 || connectorOverload > 0)
+                        everythingIsCalm = false;
+
                     List<IndicatorPhoto> list = radarPhotoResult.getIndicators(cstMesureConnectorConnectorOverloaded.name);
 
+                    milkJobOutput.addReportTable(new String[] { "Connectors Radar", "Details" });
 
-                    plugTourOutput.addReportTable(new String[] { "Connectors Radar", "Details"});
-                    
-                    plugTourOutput.addReportLine(new Object[] { "Connector call", connectorCall });
-                    plugTourOutput.addReportLine(new Object[] { "connectorOverload", connectorOverload });
-                    plugTourOutput.addReportLine(new Object[] { "Threshold (in sec)", jobExecution.getInputLongParameter(cstParamConnectorDurationInSecond) });
-                    
-                    for (IndicatorPhoto indicator : list )
-                        plugTourOutput.addReportLine(new String[] { indicator.getName(), indicator.details==null ? "" : indicator.details });
-                    
-                    plugTourOutput.addReportEndTable();
-                    
-                    plugTourOutput.addEvent( new BEvent( eventConnectorTooLongStatus, "Connectors called["+connectorCall+"] Overload["+connectorOverload+"]" ));
+                    milkJobOutput.addReportLine(new Object[] { "Connector call", connectorCall });
+                    milkJobOutput.addReportLine(new Object[] { "connectorOverload", connectorOverload });
+                    milkJobOutput.addReportLine(new Object[] { "Threshold (in sec)", jobExecution.getInputLongParameter(cstParamConnectorDurationInSecond) });
+
+                    for (IndicatorPhoto indicator : list)
+                        milkJobOutput.addReportLine(new String[] { indicator.getName(), indicator.details == null ? "" : indicator.details });
+
+                    milkJobOutput.addReportEndTable();
+
+                    milkJobOutput.addEvent(new BEvent(eventConnectorTooLongStatus, "Connectors called[" + connectorCall + "] Overload[" + connectorOverload + "]"));
 
                 }
             }
-            if (everythingIsCalm && plugTourOutput.executionStatus == ExecutionStatus.SUCCESS)
-                plugTourOutput.executionStatus = ExecutionStatus.SUCCESSNOTHING;
+            if (everythingIsCalm && milkJobOutput.executionStatus == ExecutionStatus.SUCCESS)
+                milkJobOutput.executionStatus = ExecutionStatus.SUCCESSNOTHING;
+        }
+        if (Boolean.TRUE.equals(jobExecution.getInputBooleanParameter(cstParamStatisticsProcess))) {
+            String[] listRadarsName = new String[] { RadarCase.CLASS_RADAR_NAME, RadarProcess.CLASS_RADAR_NAME };
+
+            DelayResult delayDeploymentResult = MilkPlugInToolbox.getTimeFromDelay(jobExecution, cstParamProcessDelayDeployment, new Date(), false);
+            if (BEventFactory.isError(delayDeploymentResult.listEvents)) {
+                milkJobOutput.addEvents(delayDeploymentResult.listEvents);
+                milkJobOutput.executionStatus = ExecutionStatus.ERROR;
+                return milkJobOutput;
+            }
+            
+            
+            
+            milkJobOutput.addReportTable(new String[] { "Measure", "Value" });
+
+            for (String radarName : listRadarsName) {
+                Radar radar = radarFactory.getInstance(radarName, radarName, jobExecution.getTenantId(), jobExecution.getApiAccessor());
+
+                if (radar == null) {
+                    milkJobOutput.addEvent(new BEvent(eventErrorNoRadarTrackerConnector, "Radar Worker[" + RadarWorkers.CLASS_RADAR_NAME + "] not found"));
+                    milkJobOutput.executionStatus = ExecutionStatus.ERROR;
+                    continue;
+                }
+                RadarPhotoResult result = radar.takePhoto( getRadarPhotoParameter(radar,jobExecution));
+                milkJobOutput.addEvents(result.listEvents);
+                for (RadarPhoto photo : result.listPhotos) {
+                    for (IndicatorPhoto indicator : photo.getListIndicators()) {
+                        milkJobOutput.addReportLine(new Object[] { indicator.getName(), indicator.isValue() ? indicator.getValue() : indicator.getValuePercent() });
+                    }
+                }
+            }
+            milkJobOutput.addReportEndTable();
         }
 
-        return plugTourOutput;
+        return milkJobOutput;
     }
     /* ******************************************************************************** */
     /*                                                                                  */
@@ -174,11 +219,29 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
     /*                                                                                  */
     /* ******************************************************************************** */
 
+    private RadarPhotoParameter getRadarPhotoParameter( Radar radar,MilkJobExecution jobExecution ) {
+        if (radar instanceof RadarProcess) {
+            ParameterProcess parameter=new RadarProcess.ParameterProcess();
+            DelayResult delayDeploymentResult = MilkPlugInToolbox.getTimeFromDelay(jobExecution, cstParamProcessDelayDeployment, new Date(), false);
+            
+            parameter.dateDeploymentAfter = delayDeploymentResult.delayDate.getTime();
+            return parameter;
+                    
+        }
+        return null;
+    }
+    /**
+     * 
+     * @param mesure
+     * @param radarPhotoResult
+     * @param plugTourOutput
+     * @return
+     */
     private double addMesureFromIndicator(PlugInMeasurement mesure, RadarPhotoResult radarPhotoResult, MilkJobOutput plugTourOutput) {
         List<IndicatorPhoto> list = radarPhotoResult.getIndicators(mesure.name);
         double value = 0;
         for (IndicatorPhoto photo : list) {
-                value += photo.getValue();
+            value += photo.getValueDouble(0);
         }
         plugTourOutput.setMeasure(mesure, value);
         return value;
@@ -196,7 +259,7 @@ public class MilkRadarBonitaEngine extends MilkPlugIn {
      */
     @Override
     public List<BEvent> notifyRegisterAJob(MilkJob milkJob, MilkJobExecution jobExecution) {
-        List<BEvent> listEvents = new ArrayList<>();        
+        List<BEvent> listEvents = new ArrayList<>();
 
         RadarFactory radarFactory = RadarFactory.getInstance();
 
