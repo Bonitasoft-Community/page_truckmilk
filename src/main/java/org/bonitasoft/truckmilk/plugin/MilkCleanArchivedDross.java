f1e53744d460e1d6c5c020aa8fbb1863dfbd455d
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-07-10 16:19:55 -0700

    Add perimeter scope

--- src/main/java/org/bonitasoft/truckmilk/plugin/MilkCleanArchivedDross.java
+++ src/main/java/org/bonitasoft/truckmilk/plugin/MilkCleanArchivedDross.java
@@ -9,9 +9,9 @@ import org.bonitasoft.radar.archive.RadarCleanArchivedDross.MonitorPurge;
 import org.bonitasoft.radar.archive.RadarCleanArchivedDross.TypeDrossDefinition;
 import org.bonitasoft.radar.archive.RadarCleanArchivedDross.TypeDrossExecution;
 import org.bonitasoft.truckmilk.engine.MilkJobOutput;
+import org.bonitasoft.truckmilk.engine.MilkJobOutput.Chronometer;
 import org.bonitasoft.truckmilk.engine.MilkPlugIn;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription;
-import org.bonitasoft.truckmilk.engine.MilkJobOutput.Chronometer;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription.CATEGORY;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription.JOBSTOPPER;
 import org.bonitasoft.truckmilk.job.MilkJob.ExecutionStatus;
@@ -23,16 +23,26 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
 
     static MilkLog logger = MilkLog.getLogger(MilkCleanArchivedDross.class.getName());
     private static String loggerHeader = "CleanArchivedDross ";
-    
-    private static String operationDetection = "Only detection";
-    private static String operationPurge = "Purge";
+
+    private static String cstOperationDetection = "Only detection";
+    private static String cstOperationPurge = "Purge";
+
+    private static String cstPerimeterAll = "all";
+    private static String cstPerimeterArchive = "archive";
+    private static String cstPerimeterActive = "active";
 
     private static PlugInParameter cstParamOperation = PlugInParameter.createInstanceListValues("operation", "Operation",
-            new String[] { operationDetection, operationPurge },
-            operationDetection,
+            new String[] { cstOperationDetection, cstOperationPurge },
+            cstOperationDetection,
             "Detect or clean all Drosses in archived table  "
-                    + operationDetection + ": Only detect. "
-                    + operationPurge + ": Purge records.");
+                    + cstOperationDetection + ": Only detect. "
+                    + cstOperationPurge + ": Purge records.");
+
+    private static PlugInParameter cstParamPerimeter = PlugInParameter.createInstanceListValues("perimeter", "Perimeter",
+            new String[] { cstPerimeterAll, cstPerimeterArchive, cstPerimeterActive }, cstPerimeterArchive,
+            "Perimeter of the analyse. Only archived table, or active table, or both");
+
+
 
     public MilkCleanArchivedDross() {
         super(TYPE_PLUGIN.EMBEDED);
@@ -47,7 +57,8 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
         plugInDescription.setCategory(CATEGORY.CASES);
 
         plugInDescription.addParameter(cstParamOperation);
-
+        plugInDescription.addParameter(cstParamPerimeter);
+        
         for (TypeDrossDefinition typeDross : RadarCleanArchivedDross.getListTypeDross()) {
             plugInDescription.addMesure(PlugInMeasurement.createInstance(typeDross.name, typeDross.name, typeDross.explanation).withTypeMeasure(TypeMeasure.LONG));
             String sqlQuery = typeDross.sqlQuery;
@@ -86,9 +97,10 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
         milkJobOutput.executionStatus = ExecutionStatus.SUCCESS;
 
         String operation = jobExecution.getInputStringParameter(cstParamOperation);
+        String perimeter = jobExecution.getInputStringParameter(cstParamPerimeter);
         TypeDrossDefinition[] listTypeDross = RadarCleanArchivedDross.getListTypeDross();
         // Double in case of purge
-        jobExecution.setAvancementTotalStep((long) listTypeDross.length + (operationPurge.equals(operation) ? listTypeDross.length : 0L));
+        jobExecution.setAvancementTotalStep((long) listTypeDross.length + (cstOperationPurge.equals(operation) ? listTypeDross.length : 0L));
 
         List<TypeDrossExecution> listDrossExecution = new ArrayList<>();
         int advancementStep = 0;
@@ -96,19 +108,27 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
         int totalDross = 0;
 
         //------------------------------- detection
-        if (operationDetection.equals(operation)) {
+        if (cstOperationDetection.equals(operation)) {
             for (TypeDrossDefinition typeDross : listTypeDross) {
                 if (jobExecution.pleaseStop())
                     break;
 
                 advancementStep++;
                 jobExecution.setAvancementStep(advancementStep);
-                jobExecution.setAvancementInformation("Detect "+typeDross.name+"<br><i>select count(*) "+typeDross.sqlQuery+"</i>");
                 
+                // Perimeter
+                if (! cstPerimeterAll.equals(perimeter)) {
+                    if (typeDross.onActiveTable &&  cstPerimeterArchive.equals(perimeter))
+                        continue;
+                    if (! typeDross.onActiveTable && cstPerimeterActive.equals(perimeter))
+                        continue;
+                }
+                jobExecution.setAvancementInformation("Detect " + typeDross.name + "<br><i>select count(*) " + typeDross.sqlQuery + "</i>");
+
                 Chronometer drossMarker = milkJobOutput.beginChronometer(typeDross.name);
                 TypeDrossExecution typeDrossExecution = RadarCleanArchivedDross.getStatus(jobExecution.getTenantId(), typeDross);
                 milkJobOutput.endChronometer(drossMarker);
-                
+
                 listEvents.addAll(typeDrossExecution.listEvents);
                 listDrossExecution.add(typeDrossExecution);
             }
@@ -124,30 +144,39 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
         }
 
         // ----------------------------- Purge now
-        if (operationPurge.equals(operation)) {
+        if (cstOperationPurge.equals(operation)) {
             milkJobOutput.addReportInHtml("<h4>Purge</h4><br>");
             totalDross = 0; // we want to count the number of deletion 
             // first get the
             for (TypeDrossDefinition typeDross : listTypeDross) {
                 if (jobExecution.pleaseStop()) {
-                    logger.info(loggerHeader+"Stop Asked type["+typeDross.name+"] "+jobExecution.getStopExplanation());
+                    logger.info(loggerHeader + "Stop Asked type[" + typeDross.name + "] " + jobExecution.getStopExplanation());
                     break;
                 }
 
                 advancementStep++;
                 jobExecution.setAvancementStep(advancementStep);
-                jobExecution.setAvancementInformation("Purge "+typeDross.name+"<br><i>select count(*) "+typeDross.sqlQuery+"</i>");
-
-                logger.info(loggerHeader+"Start type["+typeDross.name+"]");
                 
-                Chronometer drossMarker = milkJobOutput.beginChronometer(typeDross.name);                
+                // Perimeter
+                if (! cstPerimeterAll.equals(perimeter)) {
+                    if (typeDross.onActiveTable &&  cstPerimeterArchive.equals(perimeter))
+                        continue;
+                    if (! typeDross.onActiveTable && cstPerimeterActive.equals(perimeter))
+                        continue;
+                }
+
+                jobExecution.setAvancementInformation("Purge " + typeDross.name + "<br><i>select count(*) " + typeDross.sqlQuery + "</i>");
+
+                logger.info(loggerHeader + "Start type[" + typeDross.name + "]");
+
+                Chronometer drossMarker = milkJobOutput.beginChronometer(typeDross.name);
                 TypeDrossExecution typeDrossDeletion = RadarCleanArchivedDross.deleteDross(jobExecution.getTenantId(),
                         typeDross,
-                        new MonitorPurgeBaseOnJob(jobExecution,milkJobOutput ));
+                        new MonitorPurgeBaseOnJob(jobExecution, milkJobOutput));
                 milkJobOutput.endChronometer(drossMarker);
-                
-                logger.info(loggerHeader+"Clean type["+typeDross.name+"] Delete["+typeDrossDeletion.nbRecords+"]");
-                
+
+                logger.info(loggerHeader + "Clean type[" + typeDross.name + "] Delete[" + typeDrossDeletion.nbRecords + "]");
+
                 listEvents.addAll(typeDrossDeletion.listEvents);
                 // Already added during the stopPurgeBaseOnJob jobExecution.addManagedItems( typeDrossDeletion.nbRecords);
 
@@ -156,12 +185,13 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
 
                 totalDross += typeDrossDeletion.nbRecords;
             }
-            logger.info(loggerHeader+"Stop DrossClean: "+jobExecution.getStopExplanation());
+            logger.info(loggerHeader + "Stop DrossClean: " + jobExecution.getStopExplanation());
         }
 
         milkJobOutput.addEvents(listEvents);
-        milkJobOutput.addMeasuresInReport(true, false);
-        
+        // according the perimeter, some measure does not have a value
+        milkJobOutput.addMeasuresInReport(false, false);
+
         milkJobOutput.addChronometersInReport(false, true);
 
         milkJobOutput.setNbItemsProcessed(totalDross);
@@ -179,9 +209,10 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
 
         MilkJobExecution jobExecution;
         MilkJobOutput milkJobOutput;
-        public MonitorPurgeBaseOnJob(MilkJobExecution jobExecution, MilkJobOutput milkJobOutput ) {
+
+        public MonitorPurgeBaseOnJob(MilkJobExecution jobExecution, MilkJobOutput milkJobOutput) {
             this.jobExecution = jobExecution;
-            this.milkJobOutput = milkJobOutput;            
+            this.milkJobOutput = milkJobOutput;
         }
 
         public int getMaxNumberToProcess() {
@@ -189,7 +220,7 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
                 return jobExecution.getJobStopAfterMaxItems() - jobExecution.getManagedItems();
             }
             // no limitation on the maxNumberToProcess
-            return 100000; 
+            return 100000;
         }
 
         public boolean pleaseStop(int numberOfDeletion) {
@@ -214,7 +245,7 @@ public class MilkCleanArchivedDross extends MilkPlugIn {
 
         @Override
         public void setTimeCommitInMs(long timeInMs) {
-            milkJobOutput.addTimeChronometer("queryCommit", timeInMs);            
+            milkJobOutput.addTimeChronometer("queryCommit", timeInMs);
         }
     }
 
