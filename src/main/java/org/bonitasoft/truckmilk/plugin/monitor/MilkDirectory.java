3ffd1931bc8f5fda2a9bd18df7af41c5b02eb812
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-10-05 16:26:46 -0700

    PY Tutorial

--- src/main/java/org/bonitasoft/truckmilk/plugin/monitor/MilkDirectory.java
+++ src/main/java/org/bonitasoft/truckmilk/plugin/monitor/MilkDirectory.java
@@ -5,62 +5,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.bpm.process.ProcessDeploymentInfo;
+import org.bonitasoft.engine.bpm.process.ProcessInstance;
+import org.bonitasoft.engine.bpm.process.ProcessInstanceSearchDescriptor;
 import org.bonitasoft.log.event.BEvent;
 import org.bonitasoft.log.event.BEvent.Level;
 import org.bonitasoft.log.event.BEventFactory;
+import org.bonitasoft.truckmilk.engine.MilkJobOutput;
 import org.bonitasoft.truckmilk.engine.MilkPlugIn;
+import org.bonitasoft.truckmilk.engine.MilkPlugIn.PlugInParameter.FilterProcess;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription.CATEGORY;
 import org.bonitasoft.truckmilk.engine.MilkPlugInDescription.JOBSTOPPER;
-import org.bonitasoft.truckmilk.engine.MilkJobOutput;
-import org.bonitasoft.truckmilk.job.MilkJobExecution;
-import org.bonitasoft.truckmilk.mapper.Mapper;
-import org.bonitasoft.truckmilk.mapper.Mapper.MAPPER_POLICY;
-import org.bonitasoft.truckmilk.toolbox.CSVOperation;
-import org.bonitasoft.truckmilk.job.MilkJob;
 import org.bonitasoft.truckmilk.job.MilkJob.ExecutionStatus;
 import org.bonitasoft.truckmilk.job.MilkJobContext;
+import org.bonitasoft.truckmilk.job.MilkJobExecution;
+import org.bonitasoft.truckmilk.job.MilkJobExecution.ListProcessesResult;
 
 public class MilkDirectory extends MilkPlugIn {
 
-    private static String operationPolicyFile = "Operation on File";
-    private static String operationPolicyCsvOneCase = "Operation on CSV, One case per file";
-    private static String operationPolicyCsvOneOperationPerLine = "Operation on CSV, One case per line in the CSV";
-
-
-
-    private static String achivePolicyMoveToArchiveDirectory = "Archive";
-    private static String archivePolicyDelete = "Delete";
-    private static String archivePolicyMoveToDirectoryThenPurge = "Archive, then purge";
 
     private static PlugInParameter cstParamDirectory = PlugInParameter.createInstance("directory", "Directory", TypeParameter.STRING, "", "This directory is monitor. Each file detected in this directory which match the filter trigger an action (create a case)")
             .withMandatory(true);
 
-    private static PlugInParameter cstParamOperation = PlugInParameter.createInstanceListValues("operation", "Operation", 
-            new String[] { operationPolicyFile, operationPolicyCsvOneCase, operationPolicyCsvOneOperationPerLine },
-            operationPolicyFile, "when a file is detected, operation running. "
-            + operationPolicyFile + ": one operation (create case, execute task, ...) is performed per file. "
-            + operationPolicyCsvOneCase + ": one operation is performed per file, attributes can be read in the first CSV line."
-            + operationPolicyCsvOneOperationPerLine + ": one operation is performed per line in the CSV file; if the CSV is empty, to case are created");
+    private static PlugInParameter cstParamProcessFilter = PlugInParameter.createInstance("processfilter", "Filter on process", TypeParameter.ARRAYPROCESSNAME, null, "Job manage only process which mach the filter. If no filter is given, all processes are inspected")
+            .withFilterProcess(FilterProcess.ONLYENABLED);
 
-    private static PlugInParameter cstParamFileFilter = PlugInParameter.createInstance("fileFilter", "File Filter", TypeParameter.STRING, "*.*", "File filter, to load only file who match the filter. * and ? may be used");
-    private static PlugInParameter cstParamArchiveFilePolicy = PlugInParameter.createInstanceListValues("archivePolicy", "Archive policy",
-            new String[] { achivePolicyMoveToArchiveDirectory, archivePolicyDelete, archivePolicyMoveToDirectoryThenPurge }, 
-            achivePolicyMoveToArchiveDirectory,
-            "When the file is processed, policy to apply: "
-                    + archivePolicyDelete + ": file(s) are immediately deleted."
-                    + achivePolicyMoveToArchiveDirectory + ": file(s) are moved to the archive directory, specified in the parameters"
-                    + archivePolicyMoveToDirectoryThenPurge + ": file(s) are moved to the archive directory, then purge after a delay.");
-    private static PlugInParameter cstParamDirectoryArchive = PlugInParameter.createInstance("directoryarchive", "Directory Archive", TypeParameter.STRING, "", "When a file is used, it will be moved in this directory. If no directory is set, file is removed")
-            .withVisibleCondition(" milkJob.parametersvalue[ 'archivePolicy' ] === 'Archive' || milkJob.parametersvalue[ 'archivePolicy' ] === 'Archive, then purge'");
-    private static PlugInParameter cstParamDelayArchive = PlugInParameter.createInstanceDelay("delayarchive", "Delay before purging Archive Directory", DELAYSCOPE.MONTH, 1, "Files older than this delay in the archive directory are definitively purged")
-            .withVisibleCondition("milkJob.parametersvalue[ 'archivePolicy' ] === 'Archive, then purge'");
-    private static PlugInParameter cstParamCsvSeparator = PlugInParameter.createInstance("csvSeparator", "Csv Separator", TypeParameter.STRING, ",", "Separator expected to separate the different value (in the header, in data)");
-
-    
-    
-    Mapper mapper = new Mapper(MAPPER_POLICY.BOTH);
+    private static PlugInParameter cstParamFileFilter = PlugInParameter.createInstance("fileFilter", "File Filter", TypeParameter.STRING, "[0-z|\\s]*.doc", "File filter, to load only file who match the filter. * and ? may be used.Visit https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#sum");
 
     private static BEvent eventDirectoryNotExist = new BEvent(MilkDirectory.class.getName(), 1, Level.APPLICATIONERROR,
             "Directory not exist", "The directory given as parameters does not exist", "Jobs can't run", "Check parameters");
@@ -100,48 +75,60 @@ public class MilkDirectory extends MilkPlugIn {
 
         plugInDescription.setCategory(CATEGORY.MONITOR);
         plugInDescription.addParameter(cstParamDirectory);
-        plugInDescription.addParameter(cstParamOperation);
         plugInDescription.addParameter(cstParamFileFilter);
-        plugInDescription.addParameter(cstParamArchiveFilePolicy);
-        plugInDescription.addParameter(cstParamDirectoryArchive);
-        plugInDescription.addParameter(cstParamDelayArchive);
-        plugInDescription.addParameter(cstParamCsvSeparator);
-        plugInDescription.setStopJob( JOBSTOPPER.BOTH);
-        mapper.addPlugInParameter(plugInDescription);
+        plugInDescription.addParameter(cstParamProcessFilter);
+        plugInDescription.setStopJob(JOBSTOPPER.BOTH);
 
         return plugInDescription;
     }
 
     @Override
-    public MilkJobOutput executeJob(MilkJobExecution jobExecution) {
-        MilkJobOutput plugTourOutput = jobExecution.getMilkJobOutput();
+    public MilkJobOutput executeJob(MilkJobExecution milkJobExecution) {
+        MilkJobOutput milkJobOutput = milkJobExecution.getMilkJobOutput();
+        int numberOfCasesCreated=0;
+        int numberOfCasesInError=0;
+
         try {
-            StringBuilder analysis = new StringBuilder();
-            String operationPolicy = jobExecution.getInputStringParameter(cstParamOperation);
-            String separatorCSV = jobExecution.getInputStringParameter(cstParamCsvSeparator);
+            ProcessAPI processAPI = milkJobExecution.getApiAccessor().getProcessAPI();
 
-            List<BEvent> listEvents = mapper.initialisation(jobExecution);
-            if (BEventFactory.isError(listEvents)) {
-                plugTourOutput.addEvents(listEvents);
-                plugTourOutput.executionStatus = ExecutionStatus.ERROR;
-                return plugTourOutput;
+            // ---- get parameters
+            ListProcessesResult listProcessResult = milkJobExecution.getInputArrayProcess(cstParamProcessFilter, false, null, ProcessInstanceSearchDescriptor.PROCESS_DEFINITION_ID, processAPI);
 
+            if (BEventFactory.isError(listProcessResult.listEvents)) {
+                // filter given, no process found : stop now
+                milkJobOutput.addEvents(listProcessResult.listEvents);
+                milkJobOutput.executionStatus = ExecutionStatus.BADCONFIGURATION;
+                return milkJobOutput;
             }
-            String fileFilter = jobExecution.getInputStringParameter(cstParamFileFilter);
-
-            File fileDirectoy = new File(jobExecution.getInputStringParameter(cstParamDirectory));
+            String fileFilter = milkJobExecution.getInputStringParameter(cstParamFileFilter);
+            Pattern patternFileFilter = null;
+            if (fileFilter != null && ! fileFilter.trim().isEmpty())
+                patternFileFilter = Pattern.compile(fileFilter);
+            
+            File fileDirectoy = new File(milkJobExecution.getInputStringParameter(cstParamDirectory));
+            milkJobOutput.addReportTableBegin(new String[] { "File", "CaseId" });
+            
+            // ------------ loop now
             File[] listFiles = fileDirectoy.listFiles();
-            jobExecution.setAvancementTotalStep(listFiles.length);
+            milkJobExecution.setAvancementTotalStep(listFiles.length);
+
             for (File fileInDirectory : listFiles) {
-                jobExecution.setAvancementStep(1);
+                milkJobExecution.setAvancementStep(1);
+                
+                Thread.sleep( 10000 );
+                
+                if (milkJobExecution.isStopRequired())
+                    break;
                 if (fileInDirectory.isDirectory())
                     continue;
                 // is the file match the filter?
-                if (fileFilter!=null && fileFilter.length()>0 && ! fileInDirectory.getName().matches(fileFilter))
-                {
-                    analysis.append("File["+fileInDirectory.getName()+"] does not match;");
-                    continue;
+                if (patternFileFilter !=null) {
+                    Matcher m = patternFileFilter.matcher(fileInDirectory.getName());
+                    if (!m.matches())
+                        continue;
                 }
+
+                
                 // Ok, get it ! Create something 
                 Map<String, Object> parameters = new HashMap<>();
                 parameters.put("FILENAME", fileInDirectory.getName());
@@ -149,50 +136,37 @@ public class MilkDirectory extends MilkPlugIn {
                 List<File> fileAttachement = new ArrayList<>();
                 fileAttachement.add(fileInDirectory);
 
-                if (operationPolicyCsvOneOperationPerLine.equals(operationPolicy)) {
-                    if (! fileInDirectory.getName().toLowerCase().endsWith(".csv")) {
-                        analysis.append("File["+fileInDirectory.getName()+"] is not a CSV file;");
-                        continue;                    
+                for (ProcessDeploymentInfo processDeployment : listProcessResult.listProcessDeploymentInfo) {
+                    try {
+                        ProcessInstance processInstance = processAPI.startProcess(processDeployment.getProcessId());
+                        milkJobOutput.addReportTableLine(new Object[] { fileInDirectory.getName(), processInstance.getId() });
+                        numberOfCasesCreated++;
+                    } catch (Exception e) {
+                        milkJobOutput.addReportTableLine(new Object[] { fileInDirectory.getName(), e.getMessage() });
+                        numberOfCasesInError++;
                     }
-                    CSVOperation csvOperation = new CSVOperation();
-                    csvOperation.loadCsvDocument(jobExecution, fileInDirectory, separatorCSV);
-                    Map<String, String> record;
-                    while ((record = csvOperation.getNextRecord()) != null) {
-                        Map<String, Object> recordComplete = new HashMap<>();
-                        recordComplete.putAll(record);
-                        recordComplete.putAll(parameters);
-                        mapper.performOperation(jobExecution, recordComplete, fileAttachement);
-                    }
-                    // open the CSV
-                } else if (operationPolicyCsvOneCase.equals(operationPolicy)) {
-                    if (! fileInDirectory.getName().toLowerCase().endsWith(".csv")) {
-                        analysis.append("File["+fileInDirectory.getName()+"] is not a CSV file;");
-                        continue;                    
-                    }
-
-                    // open the CSV, complete parameters, but create a case anytime
-                    CSVOperation csvOperation = new CSVOperation();
-                    csvOperation.loadCsvDocument(jobExecution, fileInDirectory, separatorCSV);
-                    Map<String, String> record = csvOperation.getNextRecord();
-                    Map<String, Object> recordComplete = new HashMap<>();
-                    if (record != null)
-                        recordComplete.putAll(record);
-                    recordComplete.putAll(parameters);
-                    mapper.performOperation(jobExecution, recordComplete, fileAttachement );
-                } else if (operationPolicyFile.equals(operationPolicy)) {
-                    // one case per file
-                    mapper.performOperation(jobExecution, parameters, fileAttachement);
                 }
 
-                // move file to archive directory
-            }
+                // Delete the file
+                fileInDirectory.delete();
 
-            // now, purge the archive directory?
+            }
+            milkJobOutput.addReportTableEnd();
 
         } catch (Exception e) {
-
+            milkJobOutput.addReportInHtml("Exception "+e.getMessage());
         }
-        return plugTourOutput;
+        
+        // calculate the result now
+        if (numberOfCasesInError>0)
+            milkJobOutput.executionStatus = ExecutionStatus.ERROR;
+        else if (milkJobExecution.isStopRequired())
+            milkJobOutput.executionStatus = ExecutionStatus.SUCCESSPARTIAL;
+        else if (numberOfCasesCreated + numberOfCasesInError==0)
+            milkJobOutput.executionStatus = ExecutionStatus.SUCCESSNOTHING;
+        else
+            milkJobOutput.executionStatus = ExecutionStatus.SUCCESS;
+        return milkJobOutput;
     }
 
 }
